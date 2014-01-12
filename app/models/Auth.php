1a9458e3aa72964d0b6a64006e3cec216aee7190
Oleksii Herman <alex0germ@gmail.com>
2014-01-12 23:31:40 +0200

    GH17: Add captcha to login form when user tried to login 3 times in the row without success

--- app/models/Auth.php
+++ app/models/Auth.php
@@ -3,9 +3,11 @@
 class Auth {
 
 	const SESSION_TIMEOUT = 3600; //one hour
+	const MAX_RETRY_COUNT = 3;
 
 	protected $user;
 	protected $started;
+	protected $retryCount;
 
 	public function __construct($user = null) {
 		$this->user = array();
@@ -13,6 +15,7 @@ class Auth {
 			$this->saveUser($user);
 		}
 		$this->resetTimeout();
+		$this->resetRetryCount();
 	}
 
 	public function saveUser($user) {
@@ -26,11 +29,27 @@ class Auth {
 
 	public function isAuthenticated() {
 		return isset($this->user['id']) && $this->user['id']
-						&& $this->started + self::SESSION_TIMEOUT > time();
+						&& !$this->isExpired();
+	}
+
+	public function isExpired() {
+		return $this->started + self::SESSION_TIMEOUT < time();
 	}
 
 	public function resetTimeout() {
 		$this->started = time();
+	}
+
+	public function incrementRetryCount() {
+		$this->retryCount++;
+	}
+
+	public function resetRetryCount() {
+		$this->retryCount = 0;
+	}
+
+	public function isMaxRetryCount() {
+		return $this->retryCount >= self::MAX_RETRY_COUNT;
 	}
 }
 
