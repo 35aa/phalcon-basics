3ced2145c87a35e7ff3b973b2d247c358e0bec96
Oleksii Herman <alex0germ@gmail.com>
2014-01-19 19:46:53 +0200

    GH32: Email confirmation redirect

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -52,34 +52,6 @@ class UserController extends \Phalcon\Mvc\Controller {
 
 	}
 
-	public function initverifyAction() {
-		if (!$this->view->form) $this->view->setVar('form', new UserForm\InitverifyForm());
-		if (!$this->view->captcha) $this->view->setVar('captcha', new Captcha\Captcha($this->getDI()->get('config')->recaptcha));
-
-		if ($this->getDI()->getRequest()->isPost()) {
-			$initVerify = (Object) Array();
-			$usersEmailsTable = new UsersEmails();
-
-			if ($this->view->form->isValid($this->getDI()->getRequest()->getPost(), $initVerify)
-					&& $this->view->captcha->checkAnswer($this->getDI()->getRequest())
-					&& ($userEmail = $usersEmailsTable->getUnverifiedEmailByEmail($initVerify->email)) ) {
-				// send verification data
-				$userEmail->sendVerifyEmail($this->getDI()->get('config'));
-				return $this->view->pick('user/initverify_confirmation');
-			}
-		}
-	}
-
-	public function confirmemailAction() {
-		//validate get params -> goto error page
-		$form = new UserForm\VerifyEmailForm();
-		$validatedData = (Object) Array();
-		if ($form->isValid($this->getDI()->getRequest()->getQuery(), $validatedData)) {
-			$users = new Users();
-			$this->view->setVar('user', $users->verifyUserByIdAndCode($validatedData->id, $validatedData->code));
-		}
-	}
-
 	public function signinAction() {
 		if (!$this->view->form) $this->view->setVar('form', new UserForm\SigninForm());
 		if ($this->session->get('auth')->isMaxRetryCount()
