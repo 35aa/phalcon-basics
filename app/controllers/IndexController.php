468ee6cef4ca6336a40d2983a08d7e3128351230
threestarsgenius <threestarsgenius@gmail.com>
2014-02-17 20:59:57 +0000

    GH55: Remove redirect from IndexController when user not authorized

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,6 +2,8 @@
 
 class IndexController extends \Framework\AbstractController {
 
+	public function beforeExecuteRoute(\Phalcon\Mvc\Dispatcher $dispatcher) {}
+
 	public function indexAction() {
 		echo "<h1>Hello!</h1>";
 	}
