da54e50fcd3bf8bbe93b3a2fcfff16311522957c
threestarsgenius <threestarsgenius@gmail.com>
2014-04-02 11:31:30 +0000

    GH79: Use const for messages in custom validators

--- app/library/Framework/Validation/Validator/EmailExists.php
+++ app/library/Framework/Validation/Validator/EmailExists.php
@@ -3,12 +3,16 @@
 namespace Framework\Validation\Validator;
 
 class EmailExists extends \Phalcon\Validation\Validator {
+
+	const VALIDATION_MESSAGE = 'Email has already registered in the system';
+
 	public function validate ($validator, $attribute) {
 		$emailTable = new \UsersEmails();
 		if ($emailTable->isEmailRegistered($validator->getValue($attribute))) {
-			$validator->appendMessage(new \Phalcon\Validation\Message('Email has already registered in the system', $attribute, 'Email'));
+			$validator->appendMessage(new \Phalcon\Validation\Message(self::VALIDATION_MESSAGE, $attribute, 'Email'));
 			return false;
 		}
 		return true;
 	}
+
 }
--- app/library/Framework/Validation/Validator/UserExists.php
+++ app/library/Framework/Validation/Validator/UserExists.php
@@ -3,12 +3,16 @@
 namespace Framework\Validation\Validator;
 
 class UserExists extends \Phalcon\Validation\Validator {
+
+	const VALIDATION_MESSAGE = 'Username has already registered in the system';
+
 	public function validate ($validator, $attribute) {
 		$userTable = new \Users();
 		if ($userTable->isUserNameStored($validator->getValue($attribute))) {
-			$validator->appendMessage(new \Phalcon\Validation\Message('Username has already registered in the system', $attribute, 'User'));
+			$validator->appendMessage(new \Phalcon\Validation\Message(self::VALIDATION_MESSAGE, $attribute, 'User'));
 			return false;
 		}
 		return true;
 	}
+
 }
