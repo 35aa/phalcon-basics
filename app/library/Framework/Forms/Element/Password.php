0507297573ddf3a01d061e2febbe9502a98b236a
threestarsgenius <threestarsgenius@gmail.com>
2014-04-03 17:45:08 +0000

    GH89: Use const in custom form elements

--- app/library/Framework/Forms/Element/Password.php
+++ app/library/Framework/Forms/Element/Password.php
@@ -5,18 +5,29 @@ namespace Framework\Forms\Element;
 Class Password extends \Phalcon\Forms\Element\Password {
 
 	const ELEMENT_NAME = 'password';
+	const ELEMENT_CLASS = 'form-control';
+	const ELEMENT_SIZE = 15;
+	const ELEMENT_MAXLENGTH = 30;
+	const ELEMENT_VALIDATOR_PRESENCEOF_MESSAGE = 'The password is required';
+	const ELEMENT_VALIDATOR_STRINGLENGTH_MAX = 30;
+	const ELEMENT_VALIDATOR_STRINGLENGTH_MIN = 8;
+	const ELEMENT_VALIDATOR_STRINGLENGTH_MAX_MESSAGE = 'This password is too long. Please, select another one.';
+	const ELEMENT_VALIDATOR_STRINGLENGTH_MIN_MESSAGE = 'Password is too short. Minimum 8 characters required.';
 
 	public function __construct() {
-		parent::__construct(self::ELEMENT_NAME, array('class' => 'form-control', 'size' => '15', 'maxlength'=>30));
+		parent::__construct(self::ELEMENT_NAME, array(
+			'class' => self::ELEMENT_CLASS, 
+			'size' => self::ELEMENT_SIZE, 
+			'maxlength'=> self::ELEMENT_MAXLENGTH) );
 		$this->addValidators(array(
 				new \Phalcon\Validation\Validator\PresenceOf(array(
-						'message' => 'The password is required'
+						'message' => self::ELEMENT_VALIDATOR_PRESENCEOF_MESSAGE
 				)),
 				new \Phalcon\Validation\Validator\StringLength(array(
-						'max' => 30,
-						'min' => 8,
-						'messageMaximum' => 'This password is too long. Please, select another one.',
-						'messageMinimum' => 'Password is too short. Minimum 8 characters required.'
+						'max' => self::ELEMENT_VALIDATOR_STRINGLENGTH_MAX,
+						'min' => self::ELEMENT_VALIDATOR_STRINGLENGTH_MIN,
+						'messageMaximum' => self::ELEMENT_VALIDATOR_STRINGLENGTH_MAX_MESSAGE,
+						'messageMinimum' => self::ELEMENT_VALIDATOR_STRINGLENGTH_MIN_MESSAGE
 				)),
 		));
 	}
