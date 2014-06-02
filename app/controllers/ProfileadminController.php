491d1f42795a62455443f7798c5b6c05582efe45
threestarsgenius <threestarsgenius@gmail.com>
2014-06-02 20:46:03 +0000

    GH120: Implement 'Remove account' feature Fixed comments for commit 849ce5600f8e10001659fd67ca1625af0eafb71b

--- app/controllers/ProfileadminController.php
+++ app/controllers/ProfileadminController.php
@@ -11,10 +11,8 @@ class ProfileadminController extends ProfileController {
 	}
 
 	public function activateAction() {
-		if ($user = $this->_getUserByID()) {
-			$user->setUserActive();
-			return $this->dispatcher->forward(array('controller' => $this->dispatcher->getControllerName(),'action' => 'index' ));
-		}
+		if ($user = $this->_getUserByID()) $user->setUserActive();
+		return $this->dispatcher->forward(array('controller' => $this->dispatcher->getControllerName(),'action' => 'index' ));
 	}
 
 }
